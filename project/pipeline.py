79660ee44aff6111aa06c4a805a004dc877c4200
Srinivas Seema <srinivas.seema@fau.de>
2023-11-22 16:33:19 +0100

    updated data clean up & copying to database

--- project/pipeline.py
+++ project/pipeline.py
@@ -2,6 +2,7 @@ import kaggle
 import os
 import pandas as pd 
 import logging
+from sqlalchemy import create_engine
 
 
 #About Dataset
@@ -22,11 +23,17 @@ class EnergyDataPipeline:
 
     # Define a class-level logger object
     logger = logging.getLogger(__name__)
+    # Initialize energyDF and weatherDF as class attributes
+    energyDF = None
+    weatherDF = None
+    engine = None
 
 
     def __init__(self):
         # Configure the logger
         self.logger.setLevel(logging.INFO)
+        # Create a SQLAlchemy engine to connect to the SQLite database.
+        self.engine = create_engine('sqlite:///energystorm.sqlite')
     
     def downloadfiles(self):
         # Authenticate with the Kaggle API
@@ -53,23 +60,36 @@ class EnergyDataPipeline:
         # Read energy data
         energyDataPath = "./data/energy_dataset.csv"
         self.logger.info("Reading dataset %s", energyDataPath)
-        energyDF = pd.read_csv(f"{energyDataPath}")
+        self.energyDF = pd.read_csv(f"{energyDataPath}")
 
         # Preview of Energy data
-        energyDF.head(5)
+        self.energyDF.head(5)
 
         # Read Weather data
         weatherDataPath = "./data/weather_features.csv"
         self.logger.info("Reading dataset ", weatherDataPath)
-        weatherDF = pd.read_csv(f"{weatherDataPath}")
+        self.weatherDF = pd.read_csv(f"{weatherDataPath}")
 
         # Preview of Weather data
-        weatherDF.head(5)
+        self.weatherDF.head(5)
+
+    def data_exploration(self):
+        self.energyDF.info()
+        self.energyDF.describe() 
+        self.weatherDF.info()
+        self.weatherDF.describe()
+
 
     def executePipeline(self):
         #make pipeline sections
         self.downloadfiles()
         self.readfiles()
+        self.data_exploration()
+        self.writeDataToSQL()
+
+    def writeDataToSQL(self):
+        self.energyDF.to_sql('energy', self.engine, if_exists='replace',index=False)
+        self.weatherDF.to_sql('weather', self.engine, if_exists='replace',index=False)
 
     
 if __name__ == "__main__":
