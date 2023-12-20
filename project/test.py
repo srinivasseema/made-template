dbe30c5f91b02e7906fe4ba7ceb9c2e13279467b
Srinivas Seema <srinivas.seema@fau.de>
2023-12-20 13:25:20 +0100

    adding pytests replacing unittests

---
+++ project/test.py
@@ -0,0 +1,23 @@
+import pytest
+from prefect import Flow
+from pipeline import EnergyDataPipeline
+
+@pytest.fixture
+def pipeline():
+    return EnergyDataPipeline()
+
+def test_weather_flow_runs_without_errors(pipeline):
+    with pytest.raises(Exception) as excinfo:
+        weatherDF = pipeline.readWeatherData.fn()
+        pipeline.data_exploration.fn(weatherDF)
+        pipeline.null_values.fn("weather", weatherDF)
+
+    assert excinfo.value is None, "Flow raised an exception"
+
+def test_energy_flow_runs_without_errors(pipeline):
+    with pytest.raises(Exception) as excinfo:
+        energyDF = pipeline.readEnergyData.fn()
+        pipeline.data_exploration.fn(energyDF)
+        pipeline.null_values.fn("energy", energyDF)
+
+    assert excinfo.value is None, "Flow raised an exception"
