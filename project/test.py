6a99869776d8b30311a34748b41cdcaff54dd2c0
Srinivas Seema <srinivas.seema@fau.de>
2023-12-20 14:07:44 +0100

    removed unneccessary message

--- project/test.py
+++ project/test.py
@@ -10,14 +10,10 @@ def test_weather_flow_runs_without_errors(pipeline):
     with pytest.raises(Exception) as excinfo:
         weatherDF = pipeline.readWeatherData.fn()
         pipeline.data_exploration.fn(weatherDF)
-        pipeline.null_values.fn("weather", weatherDF)
-
-    assert excinfo.value is None, "Flow raised an exception"
+        pipeline.null_values.fn("weather", weatherDF) 
 
 def test_energy_flow_runs_without_errors(pipeline):
     with pytest.raises(Exception) as excinfo:
         energyDF = pipeline.readEnergyData.fn()
         pipeline.data_exploration.fn(energyDF)
         pipeline.null_values.fn("energy", energyDF)
-
-    assert excinfo.value is None, "Flow raised an exception"
